248fd92f26cfcdd68ca61bea1444680e1f519ee6
jxv <joevargas92@gmail.com>
2018-03-04 23:19:12 -0800

    generate atom feed

--- blog/gen.sh
+++ blog/gen.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-stagen clean -e header.md -f footer.md -a index.md -c style.css
-stagen build -e header.md -f footer.md -a index.md -c style.css -v "../favicon.png"
+stagen clean -e header.md -f footer.md -a index.md -c style.css -u http://jxv.io/blog -t "jxv blog"
+stagen build -e header.md -f footer.md -a index.md -c style.css -v "../favicon.png" -u http://jxv.io/blog -t "jxv blog"
